cd3821e5c8f36e70615e10799849151644c4a42b
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-01 19:31:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,7 +24,7 @@ end
        if social.attribute("href").value.include?("twitter")
          profiles_hash[:twitter] = social.attribute("href").value
       elsif social.attribute("href").value.include?("linkedin")
-        student_profile[:linkedin] = social.attribute("href").value
+        profiles_hash[:linkedin] = social.attribute("href").value
       elsif social.attribute("href").value.include?("github")
         student_profile[:github] = social.attribute("href").value
       else
