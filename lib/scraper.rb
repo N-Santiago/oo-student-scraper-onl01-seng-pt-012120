b7ef79cc8394f0d93c963c4c3033b18a5edc9c78
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-01 19:10:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,18 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    doc.css("div.student-card").each do |student|
+      student_hash = {
+        name: student.css("h4.student-name").text, 
+        location: student.css("p.student-location").text, 
+        profile_url: student.css("a").attribute("href").value 
+      }
+      students << student_hash
   end
+  students 
+end 
 
   def self.scrape_profile_page(profile_url)
     
